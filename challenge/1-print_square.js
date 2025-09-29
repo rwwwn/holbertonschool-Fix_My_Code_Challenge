5c5ed9b2e85351041621066e571690fb94c3ab12
rwwwn <ralbaraiki@gmail.com>
2025-09-29 16:45:05 +0000

    task 1: use parseInt(..., 10) with variable named size (fix base-16 to base-10)

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 // Print an N x N square of '#'
-const arg = process.argv[2];
-const n = Number.parseInt(arg, 10);
-
-if (!Number.isFinite(n) || n < 1) {
+const size = parseInt(process.argv[2], 10);
+if (!Number.isFinite(size) || size < 1) {
   process.exit(0);
 }
-
-const row = '#'.repeat(n);
-for (let i = 0; i < n; i += 1) {
+const row = '#'.repeat(size);
+for (let i = 0; i < size; i += 1) {
   console.log(row);
 }
